a352ac3488d42589f84e8fded2be1da5a4e1d0e4
Mohammad-Gouse23 <khanmohamadgouse@gmail.com>
2025-06-16 15:08:07 +0530

    Update outputs.tf

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -1,19 +1,39 @@
-output "db_endpoint" {
+output "db_instance_endpoint" {
   description = "RDS instance endpoint"
   value       = aws_db_instance.main.endpoint
 }
 
+output "db_instance_id" {
+  description = "RDS instance ID"
+  value       = aws_db_instance.main.id
+}
+
+output "db_instance_arn" {
+  description = "RDS instance ARN"
+  value       = aws_db_instance.main.arn
+}
+
+output "db_name" {
+  description = "Database name"
+  value       = aws_db_instance.main.db_name
+}
+
+output "db_username" {
+  description = "Database username"
+  value       = aws_db_instance.main.username
+}
+
 output "db_port" {
-  description = "RDS instance port"
+  description = "Database port"
   value       = aws_db_instance.main.port
 }
 
-output "security_group_id" {
-  description = "ID of the RDS security group"
-  value       = aws_security_group.rds.id
+output "db_subnet_group_name" {
+  description = "Database subnet group name"
+  value       = aws_db_subnet_group.main.name
 }
 
-output "secret_manager_arn" {
-  description = "ARN of the secrets manager secret"
+output "secret_arn" {
+  description = "ARN of the secret containing the database password"
   value       = aws_secretsmanager_secret.db_password.arn
 }

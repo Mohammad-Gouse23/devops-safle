02a46e8b6656d2cbfdb1583fc367c58680eeaadc
Mohammad-Gouse23 <khanmohamadgouse@gmail.com>
2025-06-16 15:08:50 +0530

    Update outputs.tf

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,10 +1,15 @@
-output "dns_name" {
-  description = "DNS name of the load balancer"
+output "alb_arn" {
+  description = "ARN of the Application Load Balancer"
+  value       = aws_lb.main.arn
+}
+
+output "alb_dns_name" {
+  description = "DNS name of the Application Load Balancer"
   value       = aws_lb.main.dns_name
 }
 
-output "zone_id" {
-  description = "Zone ID of the load balancer"
+output "alb_zone_id" {
+  description = "Zone ID of the Application Load Balancer"
   value       = aws_lb.main.zone_id
 }
 
@@ -13,17 +18,7 @@ output "target_group_arn" {
   value       = aws_lb_target_group.main.arn
 }
 
-output "security_group_id" {
-  description = "ID of the ALB security group"
-  value       = aws_security_group.alb.id
-}
-
-output "alb_arn_suffix" {
-  description = "ARN suffix of the ALB"
-  value       = aws_lb.main.arn_suffix
-}
-
-output "target_group_arn_suffix" {
-  description = "ARN suffix of the target group"
-  value       = aws_lb_target_group.main.arn_suffix
+output "listener_arn" {
+  description = "ARN of the listener"
+  value       = aws_lb_listener.main.arn
 }

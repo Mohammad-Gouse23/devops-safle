b38f04f4d30b5587fc2d3a249037af48c25d1262
Mohammad-Gouse23 <khanmohamadgouse@gmail.com>
2025-06-17 15:28:04 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,6 +1,6 @@
 output "load_balancer_dns" {
   description = "DNS name of the load balancer"
-  value       = module.alb.dns_name
+  value       = module.alb.alb_dns_name
 }
 
 output "ecr_repository_url" {
@@ -10,7 +10,7 @@ output "ecr_repository_url" {
 
 output "database_endpoint" {
   description = "RDS instance endpoint"
-  value       = module.rds.db_endpoint
+  value       = module.rds.rds_instance_endpoint
   sensitive   = true
 }
 
@@ -38,3 +38,13 @@ output "security_group_ids" {
   }
 }
 
+output "cloudwatch_dashboard_url" {
+  description = "URL of the CloudWatch dashboard"
+  value       = module.cloudwatch.dashboard_url
+}
+
+output "rds_secret_arn" {
+  description = "ARN of the secret storing the DB password"
+  value       = module.rds.rds_secret_arn
+  sensitive   = true
+}

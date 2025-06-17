97d74c1af0919ea51ab4da5ba45033c889e7a7f0
Mohammad-Gouse23 <khanmohamadgouse@gmail.com>
2025-06-17 15:15:53 +0530

    Update outputs.tf

--- terraform/modules/rds/outputs.tf
+++ terraform/modules/rds/outputs.tf
@@ -27,3 +27,8 @@ output "rds_db_subnet_group" {
   description = "The name of the RDS DB subnet group"
   value       = aws_db_subnet_group.main.name
 }
+
+output "db_endpoint" {
+  description = "The endpoint of the RDS instance"
+  value       = aws_db_instance.main.endpoint
+}
